8a491425ced1b80366683480cb770a69e3af86d0
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-06-02 10:48:30 -0700

    Add penguin image, add penguin divs and jQuery to slideToggle penguin image.
    Co-Authored-By: Nikolas Kososik <nikolas.kososik@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
   $('.clickable').click(function() {
-    $('#walrus-showing').toggle();
-    $('#walrus-hidden').toggle();
+    $('#walrus-showing').fadeToggle();
+    $('#walrus-hidden').fadeToggle();
+    $('#penguin-up').slideToggle();
+    $('#penguin-down').slideToggle();
   });
 });
